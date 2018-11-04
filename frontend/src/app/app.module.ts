40fd8e56456e8554ad62bcb911b650668ea182d4
Miguelmo <miguelmoyaortega@gmail.com>
2018-11-04 12:13:58 +0100

    ok

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot([
+            { path: '', component: MainComponent },
+            { path: '**', redirectTo: '' }
+        ]),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }

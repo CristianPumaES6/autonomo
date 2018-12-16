b7ed202a2c685efd51e5632817476a1f47d00ca7
Miguelmo <miguelmoyaortega@gmail.com>
2018-12-16 19:05:00 +0100

    delete and fix bug get

--- backend/src/invoices/invoices.service.ts
+++ backend/src/invoices/invoices.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { db } from '../db';
 import { Invoice } from '../db/models/invoice';
+import * as moment from 'moment';
 
 @Injectable()
 export class InvoicesService {
-    async get(userID: number) {
-        return await db.models.invoices.find({ where: { userID } });
+    async get(user: number) {
+        return await db.models.invoices.find({ where: { user } });
     }
 
-    async getID(userID: number, id: number) {
-        return await db.models.invoices.findOne({ where: { userID, id } });
+    async getID(user: number, id: number) {
+        return await db.models.invoices.findOne({ where: { user, id } });
     }
 
     async post(invoice: Invoice) {
@@ -20,5 +21,9 @@ export class InvoicesService {
     async put(invoice: Invoice, user: number) {
         delete invoice.deletedAt; delete invoice.createdAt;
         return await db.models.invoices.update({ id: invoice.id, user }, invoice);
+    }
+
+    async delete(id: number, user: number) {
+        return await db.models.invoices.update({ deletedAt: moment().format() }, { id, user });
     }
 }

b633cd2a2753bde8a33402968778741f03ed5faf
Miguelmo <miguelmoyaortega@gmail.com>
2019-02-22 20:12:02 +0100

    color border file

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -14,7 +14,6 @@ export class RegisterComponent implements OnInit {
 
     constructor(
         private authService: AuthService,
-        private route: ActivatedRoute,
         private router: Router,
     ) { }
 

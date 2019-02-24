53726a65211e76f903888d883a9eaf81f0c84fbf
Miguelmo <miguelmoyaortega@gmail.com>
2019-02-24 20:25:47 +0100

    user

--- frontend/src/app/menu/menu.component.ts
+++ frontend/src/app/menu/menu.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { MatSidenav } from '@angular/material';
 import { Router } from '@angular/router';
+import { ProfileService } from '../profile/profile.service';
+import { IUser } from '@isofocus/interfaces';
 
 @Component({
     selector: 'if-menu',
@@ -10,14 +12,20 @@ import { Router } from '@angular/router';
 })
 export class MenuComponent implements OnInit {
     logged: boolean;
+    user: IUser;
     dark = localStorage.getItem('dark') === 'true';
 
     @ViewChild('sidenav') sidenav: MatSidenav;
 
-    constructor(private readonly authService: AuthService, private readonly router: Router) { }
+    constructor(
+        private readonly authService: AuthService,
+        private readonly router: Router,
+        private readonly profileService: ProfileService,
+    ) { }
 
     ngOnInit() {
-        this.authService.logged$.subscribe(logged => this.logged = logged);
+        this.authService.logged$.subscribe((logged: boolean) => this.logged = logged);
+        this.profileService.getMy().subscribe(user => this.user = user);
     }
 
     logout() {

1b7ca71429e3914966c58930e4c277ca1875d3ab
Miguel <miguelmoyaortega@gmail.com>
2019-03-20 22:25:42 +0100

    constants

--- backend/src/app.constants.ts
+++ backend/src/app.constants.ts
@@ -20,46 +20,44 @@ import {
  * EL NOMBRE QUE TIENEN QUE TENER LAS CONSTANTES,
  * NO PONER CONTRASEÑAS EN ESTE FICHERO.
  */
-// tslint:disable-next-line:one-variable-per-declaration
-export const
-    PROD = false,
-    /**
-     * KEYS HTTPS
-     */
-    ROUTE_PRIVKEY = '/etc/letsencrypt/live/isofocus.es-0001/privkey.pem',
-    ROUTE_CERT = '/etc/letsencrypt/live/isofocus.es-0001/cert.pem',
-    /**
-     * DATABASE
-     */
-    DB_NAME = PROD ? DATABASE_PROD_NAME : DATABASE_NAME,
-    DB_USERNAME = PROD ? DATABASE_PROD_USERNAME : DATABASE_USERNAME,
-    DB_PASSWORD = PROD ? DATABASE_PROD_PASSWORD : DATABASE_PASSWORD,
-    DB_HOST = PROD ? DATABASE_PROD_HOST : DATABASE_PASSWORD,
-    DB_DIALECT = 'mysql',
-    DB_PORT = 3306,
-    /**
-     * JSON WEB TOKEN
-     */
-    JWT_SECRET = JSONWEBTOKEN_SECRET,
-    JWT_ISSUER = JSONWEBTOKEN_ISSUER,
-    JWT_EXPIRES = JSONWEBTOKEN_EXPIRES,
-    /**
-     * PORT
-     */
-    PORT = 3000,
-    /**
-     * MASTER PASSWORD TO USERS
-     */
-    MASTER_PASSWORD = LOGIN_MASTER_PASSWORD,
-    /**
-     * EMAIL
-     */
-    EMAIL_HOST = VALUE_EMAIL_HOST,
-    EMAIL_USER = VALUE_EMAIL_USER,
-    EMAIL_PASSWORD = VALUE_EMAIL_PASSWORD,
-    EMAIL_FROM = '"Isofocus" <soporte@isofocus.es>',
-    EMAIL_SERVICE = 'gmail',
-    /**
-     * SIZE
-     */
-    MAX_SIZE_FILE = 2048;
+export const PROD = false;
+/**
+ * KEYS HTTPS
+ */
+export const ROUTE_PRIVKEY = '/etc/letsencrypt/live/isofocus.es-0001/privkey.pem';
+export const ROUTE_CERT = '/etc/letsencrypt/live/isofocus.es-0001/cert.pem';
+/**
+ * DATABASE
+ */
+export const DB_NAME = PROD ? DATABASE_PROD_NAME : DATABASE_NAME;
+export const DB_USERNAME = PROD ? DATABASE_PROD_USERNAME : DATABASE_USERNAME;
+export const DB_PASSWORD = PROD ? DATABASE_PROD_PASSWORD : DATABASE_PASSWORD;
+export const DB_HOST = PROD ? DATABASE_PROD_HOST : DATABASE_PASSWORD;
+export const DB_DIALECT = 'mysql';
+export const DB_PORT = 3306;
+/**
+ * JSON WEB TOKEN
+ */
+export const JWT_SECRET = JSONWEBTOKEN_SECRET;
+export const JWT_ISSUER = JSONWEBTOKEN_ISSUER;
+export const JWT_EXPIRES = JSONWEBTOKEN_EXPIRES;
+/**
+ * PORT
+ */
+export const PORT = 3000;
+/**
+ * MASTER PASSWORD TO USERS
+ */
+export const MASTER_PASSWORD = LOGIN_MASTER_PASSWORD;
+/**
+ * EMAIL
+ */
+export const EMAIL_HOST = VALUE_EMAIL_HOST;
+export const EMAIL_USER = VALUE_EMAIL_USER;
+export const EMAIL_PASSWORD = VALUE_EMAIL_PASSWORD;
+export const EMAIL_FROM = '"Isofocus" <soporte@isofocus.es>';
+export const EMAIL_SERVICE = 'gmail';
+/**
+ * SIZE
+ */
+export const MAX_SIZE_FILE = 2048;
